c4a0621589b0a67ad5b45dd84139fb24468298a2
changxianglin <281532612@qq.com>
2018-06-05 23:37:40 +0800

    add some router & content

--- demo03/app.js
+++ demo03/app.js
@@ -36,16 +36,93 @@ app.get('/about', function (req, res) {
     res.render('about', {fortune: fortune.getFortune()})
 })
 
-app.use(function (req, res, next) {
-    res.status(404)
-    res.render('404')
+app.get('/error', function (req, res) {
+    res.status(500)
+    res.render('error')
 })
 
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500)
-    res.render('500')
+app.get('/greeting', function (req, res) {
+    res.render('about', {
+        message: 'welcome',
+        style: req.query.style,
+        userid: req.cookie.userid,
+        username: req.session.username,
+    })
 })
+
+app.get('/no-layout', function (req, res) {
+    res.render('no-layout', {layout: null})
+})
+
+app.get('/custom-layout', function (req, res) {
+    res.render('custom-layout', {layout: 'custom'})
+})
+
+app.get('/test', function (req, res) {
+    res.type('text/html')
+    res.send('this is a test')
+})
+
+app.post('/process-contact', function (req, res) {
+    console.log('Received contact from ' + req.body.name  + '<' + req.body.email + '>' )
+    res.redirect(303, '/thank-you')
+})
+
+
+app.post('/process-contact', function (req, res) {
+    console.log('Received contact from  ' + req.body.name + '<' + req.body.email + '>')
+    try{
+        return res.xhr ? res.render({success: true}) : res.redirect(303, '/thank-you')
+    } catch (ex) {
+        return res.xhr ? res.json({error: 'Database error'}) : res.redirect(303, '/database-error')
+    }
+})
+
+var tours = [
+    {id: 0, name: 'Hood River', price: 99.99},
+    {id: 1, name: 'Oregon Coast', price: 149.95},
+]
+
+app.get('/api/tours', function (req, res) {
+    res.json(tours)
+})
+
+app.put('/api/tour/:id', function (req, res) {
+    var p = tours.some(function (p) {
+        return p.id == req.params.id
+    })
+    if(p) {
+        if(req.query.name) p.name = req.query.name
+        if(req.query.price) p.price = req.query.price
+        res.json({suceess: true})
+    } else {
+        res.json({error: 'No such tour exists.'})
+    }
+})
+
+app.del('/api/tour/:id', function (req, res) {
+    var i
+    for( var i = tours.length -1 ; i >= 0; i--) {
+        if(tours[i].id === req.params.id) break
+        if(i >= 0 ) {
+            tours.splice(i, 1)
+            res.json({success: true})
+        } else {
+            res.json({error: 'No such tour exists.'})
+        }
+    }
+})
+
+// app.use(function (req, res, next) {
+//     res.status(404)
+//     res.render('404')
+// })
+
+// app.use(function (err, req, res, next) {
+//     console.error(err.stack)
+//     res.status(500)
+//     res.render('500')
+// })
 
 // app.use(function (req, res) {
 //     res.type('text/html')
@@ -59,6 +136,17 @@ app.use(function (err, req, res, next) {
 //     res.status(500)
 //     res.send('500 - Server Error')
 // })
+
+app.use(function (req, res) {
+    res.status(404).render("404")
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).render("500")
+})
+
+
 
 app.listen(app.get('port'), function () {
     console.log('running in port 5000')
