0317bc8182dbe34a664a949404e618c7d2cb55d9
changxianglin <281532612@qq.com>
2018-07-12 09:31:16 +0800

    analysis requst query

--- linkDatabase/app.js
+++ linkDatabase/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const seesion = require('express-session')
+const mongoose = require('mongoose');
 
 const app = express()
+const jsonParser = bodyParser.json()
+const urlencodeParser = bodyParser.urlencoded({extends: false})
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://blog:miniblog1@ds133601.mlab.com:33601/createblog');
 
 app.get('/', (req, res) => {
@@ -12,9 +17,28 @@ app.get('/', (req, res) => {
     res.send('link database')
 })
 
-app.get('/user/:id', function (req, res) {
-    res.download('/phone/activety618.png')
-    // res.send('OK' + app.param);
+app.get('/user/:id', urlencodeParser, function (req, res) {
+    // res.download('/phone/activety618.png')
+    res.send('OK' + req.params.id);
+})
+
+let html = `
+<form action="/post" method="get">
+1 <input name="start">
+<br>
+2 <input name="finaly">
+<br>
+<input type="submit" value="submit">
+</form>
+`
+
+app.get('/about', (req, res) => {
+    res.send(html)
+})
+
+app.get('/post', urlencodeParser, (req, res) => {
+    console.log(req.query)
+    res.send('post' + JSON.stringify(req.query))
 })
 
 
