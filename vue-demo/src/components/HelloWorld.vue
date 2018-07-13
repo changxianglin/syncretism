ad40fc4563987a8f199168cd1400418f2844530e
changxianglin <281532612@qq.com>
2018-07-13 19:27:03 +0800

    vue & font asesome & boostrap-vue

---
+++ vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <b-button-group>
+      <b-button variant="success">Success</b-button>
+      <b-button variant="info">Info</b-button>
+      <b-button variant="warning">Warning</b-button>
+      <b-button variant="primary">Primary</b-button>
+      <b-button variant="danger">Danger</b-button>
+      <b-button variant="link">Link</b-button>
+    </b-button-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vue-demo/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue'
+
+
+Vue.use(BootstrapVue)
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
