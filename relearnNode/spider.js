9ddb1d9e795bbb5f1b45e3f5d78cf83af21b3b74
changxianglin <281532612@qq.com>
2018-07-27 09:34:44 +0800

    spider use request&cheerio modules

---
+++ relearnNode/spider.js
@@ -0,0 +1,16 @@
+var request = require('request'),
+    cheerio = require('cheerio');
+
+request('https://cnodejs.org/', function(err, response, body){
+    if( !err && response.statusCode == 200 ){
+        console.log(body)
+        
+        // body为源码
+        // 使用 cheerio.load 将字符串转换为 cheerio(jQuery) 对象，
+        // 按照jQuery方式操作即可
+        var $ = cheerio.load(body);
+
+        // 输出导航的html代码
+        console.log( $('.nav').html() );
+    }
+});
