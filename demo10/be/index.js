0ce3c830b98a43998f8fd133fb5f4340f0ff8365
changxianglin <281532612@qq.com>
2018-09-11 23:47:31 +0800

    seeion

--- demo10/be/index.js
+++ demo10/be/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const app = express()
 
 app.use(cookieParser())
+app.use(session())
 app.use(bodyParser.urlencoded({extended: false}))
 
 
