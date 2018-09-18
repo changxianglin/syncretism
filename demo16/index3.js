6d23519866dcf74c5c1cd57e55c0c316274fd255
changxianglin <281532612@qq.com>
2018-09-18 19:02:58 +0800

    add some method

---
+++ demo16/index3.js
@@ -0,0 +1,11 @@
+const A = require('./test3')
+const B = require('./test3')
+const C = require('./test3')
+const D = require('./test3')
+
+// const {A, B, C, D} = require('./test3')
+
+A()
+B()
+C()
+D()
---
+++ demo16/test3.js
@@ -0,0 +1,22 @@
+function a() {
+    console.log('aaa')
+}
+
+function b() {
+    console.log('bbb')
+}
+
+function c() {
+    console.log('ccc')
+}
+
+function d() {
+    console.log('ddd')
+}
+
+
+module.exports = a
+module.exports = b
+module.exports = c
+module.exports = d
+
