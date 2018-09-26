71e375d1dbadddf1c8cf4499a603cc81e1dc34a2
changxianglin <281532612@qq.com>
2018-09-26 23:38:43 +0800

    server

---
+++ demo19/shutbox/http.js
@@ -0,0 +1,13 @@
+var http = require('http')
+
+var server = http.createServer(function(req, res) {
+
+  res.writeHead(200, {
+    'content-type': 'text/plain'
+  })
+  res.end('Hello, Node.js!')
+})
+
+server.listen(3000, function() {
+  console.log('listening port 3000')
+})
