53097a467d736d7ca54066837c8753cc41e9fe42
changxianglin <281532612@qq.com>
2018-10-08 23:46:51 +0800

    cache

---
+++ demo20/3.js
@@ -0,0 +1,30 @@
+var store = {
+  nextId: 1,
+  cache: {},
+  add: function(fn) {
+    if(!fn.id) {
+      fn.id = this.nextId++
+      this.cache[fn.id] = fn
+      return true
+    }
+  }
+}
+
+function ninja() {}
+
+function isPrime(value) {
+  if(!isPrime.answers) {
+    isPrime.answers = {}
+  }
+  if(isPrime.answers[value] !== undefined) {
+    return isPrime.answers[value]
+  }
+  var prime = value !== 0 && value !== 1
+  for(var i = 2; i < value; i++) {
+    if(value % i === 0) {
+      prime = false
+      break
+    }
+  }
+  return isPrime.answers[value] = prime
+}
