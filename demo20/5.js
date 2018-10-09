c7fc4b44aede25c9eb3b3840a3a34b15a9408012
changxianglin <281532612@qq.com>
2018-10-10 00:08:41 +0800

    function =>

--- demo20/5.js
+++ demo20/5.js
@@ -2,3 +2,16 @@ var values = [0, 3, 2, 5, 7, 4, 8, 1]
 values.sort(function(value1, value2) {
   return value1 - value2
 })
+
+var values = [0, 3, 2, 5, 7, 4, 8, 1]
+values.sort((value1, value2) => value1 - value2)
+
+param => expression
+
+var greet = name => 'Greetings ' + name
+
+
+var greet = name => {
+  var helloString = 'Greetings '
+  return helloString + name 
+}
