c808f058860303c614e754b07d8afed15127e984
changxianglin <281532612@qq.com>
2018-10-19 23:58:45 +0800

    geanetor

---
+++ demo20/24.js
@@ -0,0 +1,34 @@
+try {
+var ninjas = syncGetJSON("ninjas.json")
+var missions = syncGetJSON(ninjas[0].missionUrl)
+var missionDetails = syncGetJSON(mission[0].detailsUrl)
+}
+catch(e) {
+)
+
+
+
+
+
+
+async(function*() {
+	try {
+		const ninjas = yield getJSON('ninjas.json")
+		const missions = yield getJSON(ninjas[0].missionUrl)
+		const missionDescription = yeild getJSON(mission[0].missionUrl)
+	}
+	catch(e) {
+	
+	}
+}
+
+
+
+function* WeaponGenerator() {
+	yield "Katana"
+	yield "Wakizashi"
+	yield "Kusarigama"
+}
+
+for (let weapon of WeaponGenerator()) {
+}
